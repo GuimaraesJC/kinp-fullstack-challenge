047271acfd180a4502ad932c6798eb05685d8afc
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-02-11 14:41:53 -0300

    fix: fixes responsiviness of list of albums

--- frontend/src/pages/albums/index.tsx
+++ frontend/src/pages/albums/index.tsx
@@ -20,14 +20,13 @@ const AlbumsPage = () => {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
       <div className="max-w-screen-md mx-auto">
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {data?.map((album) => (
             <AlbumItem key={album.id} album={album} />
           ))}
         </div>
       </div>
     </div>
-
   )
 }
 
