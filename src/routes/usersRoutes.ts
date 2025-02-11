e254450c61537a9edffb428cb42de12fffdee200
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-02-10 22:58:18 -0300

    feat: implements route to fetch albums

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,15 +1,25 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import api from '../utils/axios'
 import { authenticateToken } from '../middleware/authMiddleware'
 
 const router = express.Router()
 
-router.get('/', authenticateToken,  async (req, res) => {
+router.get('/', authenticateToken, async (req: Request, res: Response) => {
   try {
     const response = await api.get('/users')
     res.json(response.data)
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' })
+  }
+})
+
+router.get('/:userId/albums', authenticateToken, async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params
+    const response = await api.get(`/users/${userId}/albums`)
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch albums' })
   }
 })
 
